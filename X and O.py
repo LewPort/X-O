3b921742a410475b7dd90d5b50dbd2351f846c2f
LewPort <porter.lewis@gmail.com>
2016-11-19 14:17:15 +0000

    Update X and O.py

--- X and O.py
+++ X and O.py
@@ -28,7 +28,7 @@ def printBoard(board): #Prints the board in its current state
     print('-+-+-')
     print(board[1] + '|' + board[2] + '|' + board[3] + '\n')
 
-def resetBoard():
+def resetBoard(): #resets all spaces on the board to ' '
     for i in board:
         board[i] = ' '
 
@@ -50,7 +50,7 @@ def coinToss(): #Tosses a virtual coin to see who goes first
 def getHumanMove(): #Player inputs their move
     global turn
     turn = 'human'
-    humanMoveIndex = int(input('Choose your move (1-9): '))
+    humanMoveIndex = 99
     while humanMoveIndex not in range(1, 10) or board[humanMoveIndex] != ' ':
         try:
             humanMoveIndex = int(input('Choose your move (1-9): '))
@@ -63,36 +63,33 @@ def getHumanMove(): #Player inputs their move
 def getComputerMove():
     global turn
     turn = computer
-    tempBoard = copy.copy(board)
+    tempBoard = copy.copy(board) #copying the current state of the board to a temporary board, on which to practice moves.
     compMoveComplete = False
 
+    #shite patter lololol
     print('\nX/0 5000 BOT IS CALCULATING MOVE\nSTAND BY FOR IMPLIMENTATION', end='')
     for i in range(1,11):
+        time.sleep(0.05)
         print('.', end = '')
-        time.sleep(0.01)
     print('\n')
 
     #Checks for winning move    
     if compMoveComplete == False:
-        print('<<Checking for winning move>>')
         for i in tempBoard: 
             if tempBoard[i] == ' ':
                 tempBoard[i] = computer
                 if aWinner(tempBoard, computer) == True:
-                    print('<<Found winning move at ' + str(board[i]) +'>>')
                     board[i] = computer
                     compMoveComplete = True
                     return
                 elif aWinner(tempBoard, computer) == False:
                     tempBoard[i] = ' '
 
-        #Checks to see if computer can block human's winning move           
-        print('<<Checking to see if opponent can win next move>>')                   
+        #Checks to see if computer can block human's winning move                  
         for i in tempBoard: 
             if tempBoard[i] == ' ':
                 tempBoard[i] = human
                 if aWinner(tempBoard, human) == True:
-                    print('<<Blocked human\'s winning move at ' + str(i) +'>>')
                     board[i] = computer
                     compMoveComplete = True
                     return
@@ -101,44 +98,32 @@ def getComputerMove():
                     
           
         #Checks to see if Centre is free
-        print('<<Checking centre>>')
         if compMoveComplete == False and board[5] == ' ':
-            print('<<Taking centre>>')
             board[5] = computer
             compMoveComplete = True
             return
 
+        #Checks to see if the corners are free
         elif compMoveComplete == False and ' ' in board[1] or ' ' in board[3] or ' ' in board[7] or ' ' in board[9]:
-            print('<<Checking corners>>') #Checks to see if a random corner is free
             corners = [1,3,7,9]
             random.shuffle(corners)
             for i in range(len(corners)):
-                print('<<Checking corner ' + str(corners[i]) + '>>')
                 if board[corners[i]] == ' ':
                     board[corners[i]] = computer
-                    print('<<found and claimed CORNER side at ' + str(corners[i]) + '>>')
                     compMoveComplete=True
                     return
-                else:
-                    print('<<NO FREE CORNERS FOUND!!!>>')
-            print('<<Finished checking Corners>>')
                     
-              
+        #Checks to see if the side squares are free             
         elif compMoveComplete == False and ' ' in board[2] or ' ' in board[4] or ' ' in board[6] or ' ' in board[8]:
-            print('<<Checking Sides>>')
-            sides = [2, 4, 6, 8] #Checks to see if the side squares are free
+            sides = [2, 4, 6, 8]
             random.shuffle(sides)
             for i in range(len(sides)):
-                print('<<Checking side ' + str(sides[i]) + '>>')
                 if board[sides[i]] == ' ':
                     board[sides[i]] = computer
                     compMoveComplete=True
-                    print('found and claimed free SIDE at ' + str(sides[i]))
                     return
-                else:
-                    print('NO FREE SIDES FOUND!!!')
 
-def aWinner(board, pl): #Winning combinations
+def aWinner(board, pl): #defining the winning combinations
         return ((board[7] == pl and board[8] == pl and board[9] == pl) or
         (board[4] == pl and board[5] == pl and board[6] == pl) or
         (board[1] == pl and board[2] == pl and board[3] == pl) or
@@ -148,14 +133,16 @@ def aWinner(board, pl): #Winning combinations
         (board[9] == pl and board[5] == pl and board[1] == pl) or
         (board[7] == pl and board[5] == pl and board[3] == pl))
 
-def playAgain():
+def playAgain(): #Do you want to play again? etc etc
+    global human
+    global computer
     playAgain = 'butts'
     while playAgain != 'Y' and playAgain != 'N':
         playAgain = input('Do you want to play again? (Y/N) ').upper()
     if playAgain == 'Y':
         resetBoard()
         human = 'butts'
-        computer = 'poop'
+        tha_GAME()
     else:
         sys.exit()
 
@@ -164,39 +151,43 @@ def playAgain():
 
 #HERE'S THE ACTUAL GAME VVVVVV
 
-while True:
-    chooseLetter()
-    if coinToss() == True:
-        printBoard(board)
-        getHumanMove()
-        printBoard(board)
-        turn = 'computer'
-    else:
-        getComputerMove()
-        turn = 'human'
-        
-    while aWinner(board, human) == False and aWinner(board, computer) == False:
-        if ' ' not in board.values():
-            print('DRAW!!!')
-            playAgain()
-        
-        elif turn == 'human':
+def tha_GAME():
+    while True:
+        chooseLetter()
+        if coinToss() == True:
             printBoard(board)
             getHumanMove()
             printBoard(board)
             turn = 'computer'
-
-        elif turn == 'computer':
+        else:
             getComputerMove()
+            printBoard(board)
             turn = 'human'
+            
+        while aWinner(board, human) == False and aWinner(board, computer) == False:
+            if ' ' not in board.values():
+                print('DRAW!!!')
+                playAgain()
+            
+            elif turn == 'human':
+                getHumanMove()
+                printBoard(board)
+                turn = 'computer'
 
-    if aWinner(board, human) == True:
-        printBoard(board)
-        print('CONGRATULATIONS! You win!')
-        playAgain()
-    elif aWinner(board, computer) == True:
-        printBoard(board)
-        for i in range(1,20):
-            print('X/O 5000 BOT IS VICTORIOUS ONCE AGAIN.')
-        playAgain()
+            elif turn == 'computer':
+                getComputerMove()
+                printBoard(board)
+                turn = 'human'
+
+        if aWinner(board, human) == True:
+            printBoard(board)
+            print('CONGRATULATIONS! You win!')
+            playAgain()
+        elif aWinner(board, computer) == True:
+            printBoard(board)
+            for i in range(1,20):
+                print('X/O 5000 BOT IS VICTORIOUS ONCE AGAIN.')
+            playAgain()
+
+tha_GAME()
         
